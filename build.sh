576365ec448548fe571903360f222d9d41e1cee8
Nir Friedman <quicknir@gmail.com>
2016-10-31 23:58:07 -0400

    Separate main compiling and linking

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ ar rcs libstatic.a static.o
 clang++ -std=c++14 -c -fpic dynamic.cpp
 clang++ -shared -o libdynamic.so dynamic.o -L./ -lstatic
 
-clang++ -std=c++14 -L./ -Wl,-rpath=./ main.x.cpp -lstatic -ldynamic
+clang++ -std=c++14 -c main.x.cpp
+clang++ -L./ -Wl,-rpath=./ main.x.o -lstatic -ldynamic
